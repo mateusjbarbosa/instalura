cd4e0d969049886925d592415735512e7c98d845
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2021-02-12 23:22:05 -0300

    Importação do botão no menu

--- src/components/commons/Menu/index.js
+++ src/components/commons/Menu/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import { MenuWrapper } from "./styles/MenuWrapper";
 
 import Logo from "../../../theme/Logo";
+import { Button } from "../Button";
 
 const links = [
   { text: "Home", url: "/" },
@@ -19,15 +20,19 @@ export default function Menu() {
       <MenuWrapper.CentralSide>
         {links.map((link) => {
           return (
-            <li>
+            <li key={link.url}>
               <a href={link.url}>{link.text}</a>
             </li>
           );
         })}
       </MenuWrapper.CentralSide>
       <MenuWrapper.RightSide>
-        <button>Entrar</button>
-        <button>Cadastrar</button>
+        <Button type="button" ghost variant="secondary.main">
+          Entrar
+        </Button>
+        <Button type="button" variant="primary.main">
+          Cadastrar
+        </Button>
       </MenuWrapper.RightSide>
     </MenuWrapper>
   );
