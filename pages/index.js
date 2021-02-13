cadc9cf7586ae03664b6827f4d1e78e181c94a5b
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2021-02-13 17:02:54 -0300

    Implementação das props no index

--- pages/index.js
+++ pages/index.js
@@ -3,23 +3,32 @@ import Menu from "../src/components/commons/Menu";
 import { Text } from "../src/components/foundation/Text";
 import { Button } from "../src/components/commons/Button";
 import { Grid } from "../src/components/layout/Grid";
+import { Box } from "../src/components/layout/Box";
 
 export default function Home() {
   return (
-    <div
-      style={{
-        flex: "1",
-        display: "flex",
-        flexWrap: "wrap",
-        flexDirection: "column",
-        justifyContent: "space-between",
-      }}
+    <Box
+      flex="1"
+      display="flex"
+      flexWrap="wrap"
+      flexDirection="column"
+      justifyContent="space-between"
+      backgroundImage="url(/images/bubbles.svg)"
+      backgroundRepeat="no-repeat"
+      backgroundPosition="bottom right"
     >
       <Menu />
 
-      <Grid.Container>
+      <Grid.Container marginTop={{ xs: "32px", md: "75px" }}>
         <Grid.Row>
-          <Grid.Col offset={1} value={{ xs: 12, md: 5 }}>
+          <Grid.Col
+            offset={1}
+            value={{ xs: 12, md: 5 }}
+            display="flex"
+            alignItems="flex-start"
+            justifyContent="center"
+            flexDirection="column"
+          >
             <div>
               <Text
                 variant="title"
@@ -68,6 +77,6 @@ export default function Home() {
       </Grid.Container>
 
       <Footer />
-    </div>
+    </Box>
   );
 }
