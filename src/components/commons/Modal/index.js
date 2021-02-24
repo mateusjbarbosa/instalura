a1c3515dcff9c3b0e545218eb4ac7ae2470305fb
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2021-02-23 22:45:15 -0300

    Adição do motion na div do Box no Modal

--- src/components/commons/Modal/index.js
+++ src/components/commons/Modal/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import PropTypes from 'prop-types';
 import styled, { css } from 'styled-components';
+import { motion } from 'framer-motion';
 
 const ModalWrapper = styled.div`
   display: flex;
@@ -15,6 +16,8 @@ const ModalWrapper = styled.div`
   bottom: 0;
   margin: auto;
   overflow: scroll;
+  transition: .3s;
+  z-index: 100;
 
   ${({ isOpen }) => {
     if (isOpen) {
@@ -43,9 +46,28 @@ function Modal({ isOpen, onClose, children }) {
         }
       }}
     >
-      {children({
-        'data-modal-safe-area': 'true',
-      })}
+      <motion.div
+        variants={{
+          open: {
+            x: 0,
+          },
+          closed: {
+            x: '-100%',
+          },
+        }}
+        animate={isOpen ? 'open' : 'closed'}
+        transition={{
+          duration: 0.5,
+        }}
+        style={{
+          display: 'flex',
+          flex: 1,
+        }}
+      >
+        {children({
+          'data-modal-safe-area': 'true',
+        })}
+      </motion.div>
     </ModalWrapper>
   );
 }
