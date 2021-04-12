d1ecee0b26a07887d58f15d7317e966db6101a49
Mateus José Barbosa <mateus.barbosa@leucotron.com.br>
2021-04-12 15:39:01 -0300

    Término do estilo da página de login

--- pages/app/login.js
+++ pages/app/login.js
@@ -1,12 +1,123 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import Button from '../../src/components/commons/Button';
+import Link from '../../src/components/commons/Link';
+
+import TextField from '../../src/components/forms/TextField';
+
+import { Text } from '../../src/components/foundation/Text';
+
+import Box from '../../src/components/layout/Box';
+import Grid from '../../src/components/layout/Grid';
+
+import { WebsitePageContext } from '../../src/components/wrappers/WebsitePage';
+
+import Logo from '../../src/theme/Logo';
 
 import websitePageHOC from '../../src/components/wrappers/WebsitePage/hoc';
 
-function LoginScreen() {
+function LoginForm() {
   return (
-    <div>
-      Login
-    </div>
+    <form id="RegisterForm" action="/app/profile/">
+      <TextField
+        placeholder="Usuário"
+        name="username"
+      />
+      <TextField
+        placeholder="Senha"
+        name="password"
+        type="password"
+      />
+
+      <Button
+        fullWidth
+        type="submit"
+        variant="primary.main"
+        margin={{
+          xs: '0 auto',
+          md: 'initial',
+        }}
+      >
+        Entrar
+      </Button>
+    </form>
+  );
+}
+
+function LoginScreen() {
+  const websitePageContext = useContext(WebsitePageContext);
+
+  return (
+    <Grid.Container
+      display="flex"
+      flex="1"
+      alignItems="center"
+    >
+      <Grid.Row
+        flex="1"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Grid.Col
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          offset={{ lg: 2 }}
+          value={{ xs: 12, md: 6, lg: 4 }}
+          flex={1}
+        >
+          <Box
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            marginTop="37px"
+            marginBottom="37px"
+          >
+            <Link
+              href="/"
+              color="secondary.main"
+            >
+              <Logo size="large" />
+            </Link>
+          </Box>
+
+          <LoginForm />
+
+          <Text
+            variant="paragraph1"
+            tag="p"
+            color="tertiary.light"
+            textAlign="center"
+          >
+            {'Não tem uma conta? '}
+            <Link
+              href="/"
+              color="secondary.main"
+              onClick={(event) => {
+                event.preventDefault();
+
+                websitePageContext.toggleModalCadastro();
+              }}
+            >
+              Cadastre-se
+            </Link>
+          </Text>
+        </Grid.Col>
+
+        <Grid.Col value={{ xs: 12, md: 6 }}>
+          <Box
+            display="flex"
+            justifyContent="center"
+          >
+            <img
+              align="center"
+              src="https://bootcamp-alura-01-git-modulo01.omariosouto.vercel.app/images/phones.png"
+              alt="Telefones mostrando as páginas internas do app"
+            />
+          </Box>
+        </Grid.Col>
+      </Grid.Row>
+    </Grid.Container>
   );
 }
 
@@ -17,6 +128,11 @@ export default websitePageHOC(LoginScreen, {
     },
     menuProps: {
       display: false,
+    },
+    pageBoxProps: {
+      backgroundImage: 'url(/images/bubbles.svg)',
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'bottom right',
     },
   },
 });
