72e493ced201b972314cb5640bf07b3f3646143b
Mateus José Barbosa <mateus.barbosa@leucotron.com.br>
2021-04-12 22:27:58 -0300

    Atualização do teste e2e de login

--- cypress/integration/pages/app/login.spec.js
+++ cypress/integration/pages/app/login.spec.js
@@ -1,13 +1,27 @@
 /// <reference types="cypress" />
 
+const API_URL = 'https://instalura-api.omariosouto.vercel.app/api/login';
+
 describe('app/login/', () => {
   it('Should fill in the fields and go to the page app/profile/', () => {
+    cy.intercept(API_URL)
+      .as('userLogin');
+
     cy.visit('/app/login/');
 
-    cy.get('#RegisterForm input[name="username"]').type('mateusjbarbosa');
+    cy.get('#RegisterForm input[name="username"]').type('omariosouto');
     cy.get('#RegisterForm input[name="password"]').type('senhasegura');
     cy.get('#RegisterForm button[type="submit"]').click();
 
     cy.url().should('include', '/app/profile');
+
+    cy.wait('@userLogin')
+      .then((intercept) => {
+        const { token } = intercept.response.body.data;
+
+        cy.getCookie('APP_TOKEN')
+          .should('exist')
+          .should('have.property', 'value', token);
+      });
   });
 });
